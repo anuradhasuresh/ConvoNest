f0a528ee50bb793eff7a525e246f690b9784b455
anuradhasuresh <55852292+anuradhasuresh@users.noreply.github.com>
2024-06-08 16:44:32 -0400

    added sign in and sign up authentication with clerk

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,29 @@
+import { ClerkProvider } from "@clerk/nextjs"
+import { Inter } from "next/font/google"
+import React from "react"
+
+import '../globals.css'
+
+// useful for SEO
+export const metadeta = {
+    title: 'Threads',
+    description: 'A Next.js 13 Meta Threads clone app'
+}
+const inter = Inter({ subsets: ["latin"] })
+
+export default function RootLayout({ 
+    children 
+}: { 
+    children: React.ReactNode 
+
+}) {
+    return (
+    <ClerkProvider>
+        <html lang= "en">
+            <body className={ '${inter.className} bg-dark-1' }>
+                { children }
+            </body>
+        </html>
+    </ClerkProvider>
+    )
+}
